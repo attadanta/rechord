8ca30c60751a98fdaaea09d7cb1cab49b82c737f
Kiril Tonev <tonev@fastmail.com>
2024-03-29 13:42:34 +0100

    Don't need a class to test.

--- util_test.py
+++ util_test.py
@@ -3,14 +3,13 @@ from util import sign
 secret = "secret"
 
 
-class TestSign:
-    def test_sign(self):
-        params = {
-            "method": "user.getRecentTracks",
-            "api_key": "api_key",
-            "foo": None,
-            "sk": "session_key",
-            "user": "user",
-        }
+def test_sign():
+    params = {
+        "method": "user.getRecentTracks",
+        "api_key": "api_key",
+        "foo": None,
+        "sk": "session_key",
+        "user": "user",
+    }
 
-        assert sign(params, secret) == "185a53fa45fb3bc0b13b757c231a0eac"
+    assert sign(params, secret) == "185a53fa45fb3bc0b13b757c231a0eac"
