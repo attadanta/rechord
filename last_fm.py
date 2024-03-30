e292f21b290b58a80a7effb41adf3262c00beaf3
Kiril Tonev <tonev@fastmail.com>
2024-03-30 11:25:14 +0100

    Parse and validate a part of the recent tracks response.

--- last_fm.py
+++ last_fm.py
@@ -1,7 +1,22 @@
 from dataclasses import dataclass
-from typing import Literal, Optional
+from datetime import datetime
+from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+
+class Timestamp(BaseModel):
+    time: datetime = Field(alias="uts")
+
+
+class Image(BaseModel):
+    size: Literal["small", "medium", "large", "extralarge"]
+    url: str = Field(alias="#text")
+
+
+class Artist(BaseModel):
+    name: str = Field(alias="#text")
+    mbid: str
 
 
 @dataclass
@@ -25,18 +40,32 @@ class GetTokenOutput(BaseModel):
     token: str
 
 
-@dataclass
-class AuthorizationInput:
-    api_key: str
-    token: str
+class Session(BaseModel):
+    """
+    The session object returned by the auth.getSession API method
+
+    See https://www.last.fm/api/show/auth.getSession
+    """
+
+    name: str
+    key: str
+    subscriber: Literal[0, 1]
+
+
+class GetSessionOutput(BaseModel):
+    """
+    The session object returned by the auth.getSession API method
+
+    See https://www.last.fm/api/show/auth.getSession
+    """
+
+    session: Session
 
 
 @dataclass
 class GetRecentTracksInput:
     user: str
-    api_key: str
     from_date: int
     to_date: int
-    session_key: Optional[str] = None
     limit: int = 20
     extended: Literal[0, 1] = 0
--- last_fm_test.py
+++ last_fm_test.py
@@ -1,20 +1,37 @@
 from dataclasses import asdict
+from pydantic import ValidationError
+from pytest import raises as pytest_raises
+from datetime import datetime, timezone
 
-from last_fm import GetRecentTracksInput
+from last_fm import GetRecentTracksInput, Image, Timestamp
 
 
-class TestLastFM:
-    def test_recent_tracks_input_data_class_dict(self):
-        i = GetRecentTracksInput(
-            user="user", api_key="api_key", from_date=1, to_date=2, extended=1
-        )
-        expected = {
-            "user": "user",
-            "api_key": "api_key",
-            "session_key": None,
-            "from_date": 1,
-            "to_date": 2,
-            "limit": 20,
-            "extended": 1,
-        }
-        assert asdict(i) == expected
+def test_recent_tracks_input_data_class_dict():
+    i = GetRecentTracksInput(user="user", from_date=1, to_date=2, extended=1)
+    expected = {
+        "user": "user",
+        "from_date": 1,
+        "to_date": 2,
+        "limit": 20,
+        "extended": 1,
+    }
+    assert asdict(i) == expected
+
+
+def test_timestamp_deserialization():
+    data = {"uts": "1711782712", "#text": "30 Mar 2024, 07:11"}
+    timestamp = Timestamp(**data)
+    assert timestamp.time == datetime(2024, 3, 30, 7, 11, 52, tzinfo=timezone.utc)
+
+
+def test_image_deserialization():
+    data = {"size": "small", "#text": "https://last.fm/image.jpg"}
+    image = Image(**data)
+    assert image.size == "small"
+    assert image.url == "https://last.fm/image.jpg"
+
+
+def test_image_deserialization_invalidates():
+    with pytest_raises(ValidationError):
+        data = {"size": "smol", "#text": "https://last.fm/image.jpg"}
+        Image(**data)
