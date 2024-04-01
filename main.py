bf41ecfb205ce7ad183b080fd8fbaa93474b89e3
Kiril Tonev <tonev@fastmail.com>
2024-04-01 16:13:42 +0200

    Got it on the first try.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from dataclasses import dataclass
+
 from fastapi import FastAPI
 from last_fm_model import (
     Track, Album
 )
-from stats import unique_albums
+from stats import unique_albums, tracks_in_album
 
 from util import load_tracks_data
 
 tracks = load_tracks_data("data")
-albums = list(unique_albums(tracks))
+albums = list(album for album in unique_albums(tracks) if album.mbid != "")
+
+@dataclass
+class AlbumDetails:
+    name: str
+    artist: str
+    tracks: list[Track]
 
 
 api = FastAPI()
@@ -19,5 +27,6 @@ async def albums_index():
 
 
 @api.get("/albums/{album_id}")
-async def album_detail(album_id: int) -> Album:
-    return albums[album_id]
+async def album_details(album_id: int) -> Album:
+    album = albums[album_id]
+    album_tracks = tracks_in_album(tracks, album)
--- stats.py
+++ stats.py
@@ -31,3 +31,40 @@ def unique_albums(tracks: Sequence[Track]) -> set[Album]:
     A list of unique album names.
     """
     return set(track.album for track in tracks)
+
+
+def sort_by_time_played(track: Track) -> datetime:
+    """
+    Given a track, return the time it was played.
+    """
+    return track.timestamp.time
+
+
+def tracks_in_album(tracks: Sequence[Track], album: Album) -> list[Track]:
+    """
+    Given a list of tracks and an album, return a list of tracks that belong to that album.
+
+    Args:
+    tracks: A list of Track objects.
+    album: An Album object.
+
+    Returns:
+    A list of Track objects that belong to the given album.
+    """
+    return [track for track in tracks if track.album == album]
+
+
+def first_and_last_listen(tracks: Sequence[Track]) -> tuple[datetime, datetime]:
+    """
+    Given a sequence of tracks, return the first and last time played.
+    """
+    if not tracks:
+        return (None, None)
+
+    if len(tracks) == 1:
+        return tracks[0].timestamp.time, tracks[0].timestamp.time
+
+    sorted_tracks = sorted(tracks, key=sort_by_time_played)
+    first, last = sorted_tracks[0], sorted_tracks[-1]
+
+    return (first.timestamp.time, last.timestamp.time)
